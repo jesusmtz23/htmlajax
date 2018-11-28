48eaa94749134a577db109c7a5cda0fbb3b7926e
jezusmtz <systemaskom@gmail.com>
2018-11-28 12:58:08 -0600

    se agrego la funcion de listado en js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
-alert();
+//Obtener la referencia del evento
+var btnActualizar = document.getElementById('btnActualizar');
+
+//Agregar un event listener para cada click
+btnActualizar.addEventListener('click',actualizar);
+
+function actualizar()
+{
+  alert('actualizar el listado');
+}
